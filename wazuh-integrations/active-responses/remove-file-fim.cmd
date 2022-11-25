619bd2e7f2c42d06926e99bd47ab51dd0c0a53ee
Darío Menten <dariomenten@gmail.com>
2022-11-25 15:00:58 -0300

    update-remove-fim

--- wazuh-integrations/active-responses/remove-file-fim.cmd
+++ wazuh-integrations/active-responses/remove-file-fim.cmd
@@ -1,8 +1,12 @@
 @ECHO OFF
 
 powershell -executionpolicy ByPass ^
+    $log_file = 'active-responses.log'; ^
     $alert = Read-Host; ^
     $alert_dict = ConvertFrom-Json $alert; ^
+    $alert_id = $alert_dict.parameters.alert.id; ^
+    $log_line = (Get-Date).ToString('yyyy-MM-dd hh:mm:ss')+' active-response/bin/remove-file-fim.cmd: Starting with Alert ID '+$alert_id; ^
+    Add-Content -Path $log_file -Value $log_line; ^
     $filename = $alert_dict.parameters.alert.syscheck.path; ^
     $filesize = $alert_dict.parameters.alert.syscheck.size_after; ^
     $filehash = $alert_dict.parameters.alert.syscheck.md5_after; ^
@@ -12,7 +16,9 @@ powershell -executionpolicy ByPass ^
         if ($hash.Hash -eq $filehash) { ^
             Remove-Item -Path $hash.Path -ErrorVariable result -ErrorAction SilentlyContinue; ^
             $log_line = (Get-Date).ToString('yyyy-MM-dd hh:mm:ss')+' active-response/bin/remove-file-fim.cmd: '; ^
-            if ($result.Count -eq 0) {$log_line += $hash.Path+' deleted'} else {$log_line += 'Unable to delete. Reason: '+$result[0].CategoryInfo.Reason}}; ^
-            Add-Content -Path 'C:\Program Files (x86)\ossec-agent\active-response\active-responses.log' -Value $log_line}
+            if ($result.Count -eq 0) {$log_line += $hash.Path+' deleted'} else {$log_line += 'Unable to delete. Reason: '+$result[0].CategoryInfo.Reason}} ^
+            Add-Content -Path $log_file -Value $log_line}; ^
+    $log_line = (Get-Date).ToString('yyyy-MM-dd hh:mm:ss')+' active-response/bin/remove-file-fim.cmd: Ended'; ^
+    Add-Content -Path $log_file -Value $log_line
 
 :Exit
